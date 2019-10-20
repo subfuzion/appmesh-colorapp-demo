d2aaab4fe33ee1141407028d64f7a6d842a888f7
Tony Pujals <tpujals@gmail.com>
2019-10-19 19:10:26 -0700

    update readme, restructure, and refactor xbuild.sh

---
+++ colorapp/README.md
@@ -0,0 +1,23 @@
+# Color App demo for App Mesh
+
+This repo contains the Go source code that supports the documentation guide at [appmesh.dev](https://appmesh.dev).
+
+The **Color App** is a simple microservice application that is useful for demonstrate core App Mesh traffic management and observability features. It consists of two services, a frontend service (`gateway`) that listens for HTTP requests at `/color` and makes a call to a backend service (`colorteller`) that responds with a configured color.
+
+Each version of `colorteller` responds with different color. The purpose of the demo is to showcase App Mesh features for controlling traffic routing simply and seamlessly in a way that is transparent to the application.
+
+This repo contains the following:
+* A frontend microservice, called `gateway`
+* A backend microservice, called `colorteller`
+* A CLI, called `colorapp`, that makes it easy deploy the full stack to Fargate, including public load balancer, and experiment with routing. The point is to facilitate demos without getting bogged down excessively in distracting low level configuration and deployment details that detract from comprehension.
+
+To install the Color App demo CLI on your system:
+
+* If you have Go 1.12+ installed on your system, simply run
+
+    $ go get github.com/subfuzion/appmesh-colorapp-demo/...
+
+* Download the latest release for Linux (linux/amd64), macOS (darwin/amd64), or Windows (windows/amd64) from
+
+    [appmesh.dev/releases](https://github.com/subfuzion/appmesh.dev/releases).
+
--- colorapp/scripts/xbuild.sh
+++ colorapp/scripts/xbuild.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# creates both *.zip and *.tar.gz for specified platform binaries
-platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+# This script will create both *.zip and *.tar.gz for platform binaries, below.
+platforms=(
+  "linux/amd64"
+  "darwin/amd64"
+  "windows/amd64"
+)
 
 SRC=$1
 DEST=$2
@@ -11,16 +15,15 @@ VERSION=$4
 i=0
 for arg in "SRC" "DEST" "NAME" "VERSION"; do
   (( i++ ))
-  if [ -z ${!arg} ]; then
-    echo "error: missing arg #$i: $arg"
+  if [ -z "${!arg}" ]; then
+    echo "error: missing arg #$i: ${arg}"
     echo "Usage xbuild SRC DEST APPNAME VERSION"
     exit 1
   fi
 done
 
-# remove extra slashes and then remove trailing
 fixpath() {
-  echo $1 | sed 's,//*,/,g' | sed 's,/$,,g'
+  echo "$1" | sed 's,//*,/,g' | sed 's,/$,,g'
 }
 
 for platform in "${platforms[@]}"; do
@@ -28,26 +31,26 @@ for platform in "${platforms[@]}"; do
   GOOS=${split[0]}
   GOARCH=${split[1]}
 
-  pkg="${NAME}-${GOOS}-${GOARCH}-${VERSION}"
-  fname="${pkg}"
+  pkgname="${NAME}-${GOOS}-${GOARCH}-${VERSION}"
+  filename="${pkgname}"
 
-  if [ "$GOOS" = "windows" ]; then
-    fname+=".exe"
+  if [ "${GOOS}" = "windows" ]; then
+    filename+=".exe"
   fi
 
-  zfile="${pkg}.zip"
-  zpath="$(fixpath ${DEST})/${zfile}"
+  zipfile="${pkgname}.zip"
+  zippath=$(fixpath "${DEST}")/"${zipfile}"
 
-  tfile="${pkg}.tar.gz"
-  tpath="$(fixpath ${DEST})/${tfile}"
+  tarfile="${pkgname}.tar.gz"
+  tarpath=$(fixpath "${DEST}")/"${tarfile}"
 
   set -e
   tmpdir=$(mktemp -d)
-  env GOOS="$GOOS" GOARCH="$GOARCH" go build -o "$tmpdir/$fname" "$SRC"
+  env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${tmpdir}/${filename}" "${SRC}"
   mkdir -p "$DEST"
-  zip -j "$zpath" "$tmpdir/$fname" >/dev/null
-  tar -czf "$tpath" -C "$tmpdir" .
-
-  rm -rf $tmpdir 2&1>/dev/null
+  zip -j "${zippath}" "${tmpdir}/${filename}" >/dev/null
+  tar -czf "${tarpath}" -C "${tmpdir}" .
+  rm -rf "${tmpdir}" 2&1>/dev/null
+  set +e
 done
 
